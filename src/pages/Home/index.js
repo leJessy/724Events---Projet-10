15d2c3c909dd5a2a67600715ffa4a3efb4ffbca4
Jessy <ayy.jessy@gmail.com>
2023-09-10 10:02:09 +0200

    Affichage img notre dernière prestation

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,7 +13,9 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData()
+  const {data} = useData();
+  //
+  const last = data && data.events && data.events.length > 0 ? data.events[data.events.length -1] : null;
   return <>
     <header>
       <Menu />
@@ -118,10 +120,13 @@ const Page = () => {
         <h3>Notre derniére prestation</h3>
         <EventCard
           imageSrc={last?.cover}
+          //
+          imageAlt={last?.description}
           title={last?.title}
           date={new Date(last?.date)}
           small
-          label="boom"
+          //
+          label={last?.type}
         />
       </div>
       <div className="col contact">
